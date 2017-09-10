f167a65c77f3eb26f54825039e7b3d874e5fc89a
PG408 <peter.gao418@hotmail.com>
2017-09-10 23:19:57 +0800

    Up date
    for better reading

--- UNIT1/Lecture 2 - Decision Trees and Dynamic Programming/brute_force_algorithms_vs_greedy_algoritems.py
+++ UNIT1/Lecture 2 - Decision Trees and Dynamic Programming/brute_force_algorithms_vs_greedy_algoritems.py
@@ -99,12 +99,25 @@ def testMaxVal(foods, maxUnits, printItems = True):
     if printItems:
         for item in taken:
             print('--', item)
-    
-names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'cola', 'apple', 'donut', 
-         'cake']
-values = [89,90,95,100,90,79,50,10]
-calories = [123,154,258,354,365,150,95,195]
-foods = buildMenu(names, values, calories)
-testGreedys(foods, 750)
-print('')
-testMaxVal(foods, 750)
+
+import random
+
+def buildLargeMenu(numItems, maxVal, maxCost):
+    items = []
+    for i in range(numItems):
+        items.append(Food(str(i),
+                          random.randint(1, maxVal),
+                          random.randint(1,maxCost)))
+    return items
+
+for numItems in range(5,46,5):
+    items = buildLargeMenu(numItems, 90, 250)
+    testMaxVal(items, 750, False)
+#names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'cola', 'apple', 'donut', 
+#         'cake']
+#values = [89,90,95,100,90,79,50,10]
+#calories = [123,154,258,354,365,150,95,195]
+#foods = buildMenu(names, values, calories)
+#testGreedys(foods, 750)
+#print('')
+#testMaxVal(foods, 750)
